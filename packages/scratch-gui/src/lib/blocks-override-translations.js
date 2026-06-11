463611231d88e939e849dd4108ab268f99810a3d
yokobond <koji.yokokawa@gmail.com>
2026-06-11 10:38:29 +0900

    feat: improve readability of Japanese operators

--- packages/scratch-gui/src/lib/blocks-override-translations.js
+++ packages/scratch-gui/src/lib/blocks-override-translations.js
@@ -3,15 +3,15 @@
 const blockOverrideTranslations = {
     'ja': {
         OPERATORS_ADD: '%1 ＋ %2',
-        OPERATORS_SUBTRACT: '%1 − %2',
+        OPERATORS_SUBTRACT: '%1 － %2',
         OPERATORS_MULTIPLY: '%1 ✕ %2',
-        OPERATORS_DIVIDE: '%1 ÷ %2'
+        OPERATORS_DIVIDE: '%1 ／ %2'
     },
     'ja-Hira': {
         OPERATORS_ADD: '%1 ＋ %2',
-        OPERATORS_SUBTRACT: '%1 − %2',
+        OPERATORS_SUBTRACT: '%1 － %2',
         OPERATORS_MULTIPLY: '%1 ✕ %2',
-        OPERATORS_DIVIDE: '%1 ÷ %2'
+        OPERATORS_DIVIDE: '%1 ／ %2'
     }
 };
 
